7c9686aca8dac09e45e36666d38a4cfb416e2444
Felipe Blanco <felipeblan@gmail.com>
2022-02-05 17:50:59 -0500

    Menu items and json data

---
+++ src/app/interfaces/menu-item.interface.ts
@@ -0,0 +1,6 @@
+export interface MenuItemI {
+  title: string;
+  icon: string;
+  path: string;
+  chidren?: Array<any> | any;
+}
--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { isPlatform } from '@ionic/angular';
+import { MenuItemI } from 'src/app/interfaces/menu-item.interface';
+import categoryData from 'src/assets/company/categories.json';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.page.html',
   styleUrls: ['./menu.page.scss'],
 })
+
 export class MenuPage implements OnInit {
+
+  menuItems: Array<MenuItemI> = [
+    {
+      title: "Home", 
+      icon: "home", 
+      path: "/"
+    },
+    {
+      title: "Products", 
+      icon: "list", 
+      path: "/products",
+      chidren: categoryData
+    },
+    {
+      title: "About", 
+      icon: "information", 
+      path: "/about"
+    },
+  ];
+
+  title: string = "Home";
 
   constructor() { }
 
   ngOnInit() {
+    const headerHeight = isPlatform("ios") ? 44 : 56;
+    document.documentElement.style.setProperty(
+      "--header-height",
+      `${headerHeight}px`
+    );
+  }
+
+  setTitle(title: string) {
+    this.title = title;
   }
 
 }
